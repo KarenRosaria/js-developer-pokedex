4fd665632630e0e61ac9b1d75a0df92a13e563e7
karen <karenrosaria33@gmail.com>
2024-01-05 22:52:40 -0300

    minhas alterações

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,35 +1,28 @@
+const pokeApi = {};
 
-const pokeApi = {}
-
-function convertPokeApiDetailToPokemon(pokeDetail) {
-    const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.id
-    pokemon.name = pokeDetail.name
-
-    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
-    const [type] = types
-
-    pokemon.types = types
-    pokemon.type = type
-
-    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
-
-    return pokemon
+pokeApi.getPokemons = (offset,limit) => {
+    const Url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+    return fetch(Url)
+    .then((response) => response.json())
+    .then((responseBody) => responseBody.results)
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+    .then((detailRequests) => Promise.all(detailRequests))
+    .then((pokemonDetails) => pokemonDetails)   
+    .catch((error) => console.error(error));
 }
 
 pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
-        .then((response) => response.json())
-        .then(convertPokeApiDetailToPokemon)
+            .then((response) => response.json())
+            .then(pokeApi.convertPokemonDetailToPokemonModel);
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 5) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
-    return fetch(url)
-        .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results)
-        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-        .then((detailRequests) => Promise.all(detailRequests))
-        .then((pokemonsDetails) => pokemonsDetails)
+pokeApi.convertPokemonDetailToPokemonModel = (pokemonDetail) => {
+    const pokemon = new Pokemon();
+    pokemon.number = pokemonDetail.order;
+    pokemon.name  = pokemonDetail.name;
+    pokemon.types = pokemonDetail.types.map((typeSlot) => typeSlot.type.name);
+    pokemon.type = pokemon.types[0];
+    pokemon.photo = pokemonDetail.sprites.other.dream_world.front_default;
+    return pokemon;
 }
